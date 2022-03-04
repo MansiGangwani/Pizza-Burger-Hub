f486f8f19427354695f26bf7fe01f37923d144eb
Mansi Thakkar <mansithakkar311@gmail.com>
2022-03-05 01:13:50 +0530

    Add project files.

---
+++ Pizza Burger Hub/Data/Migrations/20220304185051_m1.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Pizza_Burger_Hub.Data.Migrations
+{
+    public partial class m1 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
---
+++ Pizza Burger Hub/Data/Migrations/20220304192802_m2.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Pizza_Burger_Hub.Data.Migrations
+{
+    public partial class m2 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
